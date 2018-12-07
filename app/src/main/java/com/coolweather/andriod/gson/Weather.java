eaa7e503b1bc3d5c80da4ce4de4454a8538eb9f6
Your Name <you@example.com>
2018-12-07 11:12:51 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/coolweather/andriod/gson/Weather.java
@@ -0,0 +1,8 @@
+package com.coolweather.andriod.gson;
+
+/**
+ * Created by Administrator on 2018/12/7.
+ */
+
+public class Weather {
+}
---
+++ app/src/main/java/com/coolweather/andriod/until/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.coolweather.andriod.until;
+
+/**
+ * Created by Administrator on 2018/12/7.
+ */
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
